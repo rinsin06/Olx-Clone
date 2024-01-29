649ba64fe8541f2b7f9a748f8409df7377371737
rinsin06 <129921475+rinsin06@users.noreply.github.com>
2024-01-29 23:37:41 +0530

    Update config.js
    Deleted the firbase keys

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,13 +5,13 @@ import 'firebase/storage'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCHQZMmD9iLukUv5kAP2mDklCV0xIq7WAg",
-    authDomain: "olx-app-f855d.firebaseapp.com",
-    projectId: "olx-app-f855d",
-    storageBucket: "olx-app-f855d.appspot.com",
-    messagingSenderId: "167397081850",
-    appId: "1:167397081850:web:be0686c7c3334f338cff43",
-    measurementId: "G-2N6BTQCMF5"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 
  export default firebase.initializeApp(firebaseConfig)
